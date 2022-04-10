88782ef17d3f9847690e170a59634d4a0d9b8a78
Bogs Espinas <jrpespinas@gmail.com>
2022-04-10 22:54:38 +0800

    feat: implement routing for users

---
+++ server/controllers/users.js
@@ -0,0 +1,92 @@
+/**
+ * Updates a goal given an id.
+ *
+ * @access private
+ * @route GET /api/v1/users
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Not implemented",
+  });
+};
+
+/**
+ * Updates a goal given an id.
+ *
+ * @access private
+ * @route GET /api/v1/users/:id
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Not implemented",
+  });
+};
+
+/**
+ * Updates a goal given an id.
+ *
+ * @access private
+ * @route PATCH /api/v1/users/:id
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Not implemented",
+  });
+};
+
+/**
+ * Updates a goal given an id.
+ *
+ * @access private
+ * @route DELETE /api/v1/users/:id
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Not implemented",
+  });
+};
+
+/**
+ * Updates a goal given an id.
+ *
+ * @access private
+ * @route POST /api/v1/users
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Not implemented",
+  });
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  createUser,
+};
