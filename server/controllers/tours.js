b2439cc2ff24c4da146c731ade44759523fa725b
Bogs Espinas <jrpespinas@gmail.com>
2022-04-15 22:47:59 +0800

    feat: add new controller for unwind

--- server/controllers/tours.js
+++ server/controllers/tours.js
@@ -195,6 +195,58 @@ const getTourStats = async (req, res) => {
   }
 };
 
+const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = parseInt(req.params.year);
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourSTarts: -1 },
+      },
+      // {
+      //   $limit: 12,
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: plan.length,
+      data: { plan },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 module.exports = {
   getAllTours,
   getTour,
@@ -202,4 +254,5 @@ module.exports = {
   deleteTour,
   createTour,
   getTourStats,
+  getMonthlyPlan,
 };
--- server/routes/tours.js
+++ server/routes/tours.js
@@ -4,15 +4,18 @@ const {
   getAllTours,
   getTourStats,
   getTour,
+  getMonthlyPlan,
   updateTour,
   deleteTour,
   createTour,
 } = require('../controllers/tours');
 const { aliasTopTours } = require('../middleware/tours');
 
-router.route('/').get(getAllTours).post(createTour);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/tours-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
+
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
