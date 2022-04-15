4dae471c2b3fe535a31a291e001c72780173354d
Bogs Espinas <jrpespinas@gmail.com>
2022-04-15 21:54:41 +0800

    feat: implement aggregation

--- server/controllers/tours.js
+++ server/controllers/tours.js
@@ -146,9 +146,47 @@ const deleteTour = async (req, res) => {
   }
 };
 
+/**
+ * Obtain insights from the data
+ *
+ * @access private
+ * @route GET /api/v1/tours/tours-stats
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
 const getTourStats = async (req, res) => {
   try {
-    const stats = Tour.aggregate([]);
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
--- server/routes/tours.js
+++ server/routes/tours.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllTours,
+  getTourStats,
   getTour,
   updateTour,
   deleteTour,
@@ -11,6 +12,7 @@ const { aliasTopTours } = require('../middleware/tours');
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
+router.route('/tours-stats').get(getTourStats);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
